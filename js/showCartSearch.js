80cdaa44680829bdf5ec3185ac928a8e5138f8de
hoanhngjp <hoanhnghiep2704@gmail.com>
2024-03-22 02:35:40 +0700

    them sidebar va boc mainbody

---
+++ js/showCartSearch.js
@@ -0,0 +1,29 @@
+var mainBody = document.getElementById('main-body');
+var siteNav = document.getElementById('site-nav');
+var openSearch = document.getElementById('open-site-search');
+var openCart = document.getElementById('open-site-cart');
+var siteSearch = document.getElementById('site-search');
+var siteCart = document.getElementById('site-cart');
+var siteCloseHandle = document.getElementById('site-close-handle');
+
+document.addEventListener("DOMContentLoaded", function() {
+    openSearch.addEventListener("click", function(event) {
+        event.preventDefault();
+        siteNav.classList.add("active");
+        siteSearch.style.display = 'block';
+    })
+
+
+    openCart.addEventListener("click", function(event) {
+        event.preventDefault();
+        siteNav.classList.add("active");
+        siteCart.style.display= 'block';
+    })
+
+    siteCloseHandle.addEventListener("click", function(event) {
+        event.preventDefault();
+        siteNav.classList.remove("active");
+        siteSearch.classList.remove("showSearch");
+        siteCart.classList.remove("showCart");
+    })
+})
