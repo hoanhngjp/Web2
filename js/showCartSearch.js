ccd3a66b4009403965243ca3087f91d591bc388f
hoanhngjp <hoanhnghiep2704@gmail.com>
2024-03-22 14:12:13 +0700

    finish accoount site layout

--- js/showCartSearch.js
+++ js/showCartSearch.js
@@ -5,12 +5,14 @@ var openCart = document.getElementById('open-site-cart');
 var siteSearch = document.getElementById('site-search');
 var siteCart = document.getElementById('site-cart');
 var siteCloseHandle = document.getElementById('site-close-handle');
+var mainBody = document.getElementById('main-body');
 
 document.addEventListener("DOMContentLoaded", function() {
     openSearch.addEventListener("click", function(event) {
         event.preventDefault();
         siteNav.classList.add("active");
         siteSearch.style.display = 'block';
+        mainBody.classList.add("site-active");
     })
 
 
